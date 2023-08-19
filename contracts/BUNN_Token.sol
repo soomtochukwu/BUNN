af7cce378ba7da13d82576611fbf3f0fba28b452
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2023-08-19 16:00:47 +0100

    added the allow() function.

--- contracts/BUNN_Token.sol
+++ contracts/BUNN_Token.sol
@@ -9,13 +9,21 @@ contract Onwuajuese {
     uint8 public  decimal = 18;
     uint256 public  totalSupply;
 
-    constructor() {
-        _mint(msg.sender, 1234e18);
-    }
-
     mapping (address => uint256) private users; // alias: "BalanceOf" ... 
     mapping (address => mapping (address => uint256)) public allowance;
 
+    constructor() {
+        _mint(msg.sender, 1234e18);
+    }
+    /*
+    function allow()...
+    this function allows me to determine the amount of funds a
+    particula addres can transfer unbehalf of another.
+    In other words, it assigns value to the ALLOWANCE MAP at line 13
+    */ 
+    function allow(address addressFrom, address addressTO, uint256 allowed ) public  {
+        allowance[addressFrom][addressTO] = allowed;
+    }
     /*
     function balanceOf()
     it allows an instance of the contract to store and return
@@ -34,41 +42,42 @@ contract Onwuajuese {
         users[address_to] += amount;
     }
     /*
+    function _transfer()...
+    this is a boiler plate to reduce CODE DUPLICATION
+    */
+    function _transfer(address addressFrom, address addressTo, uint256 amount) private returns (bool) {
+        // local variables 
+        uint256 currentBalance = users[addressFrom];
+        // Snaity check
+        require(addressTo != address(0), "ERC20 transfer to addres 0.");
+        require(amount <= currentBalance, "Insufficient balance.");
+        // code(s)
+        users[addressFrom] -= amount; // currentAllowance -= amount; ... doeas not work
+        users[addressTo] += amount;
+        return true;
+    }
+    /*
     function transfer()...
     this function allows a certian amount of token
     to be transferred from the total supply to an addres
     */
-    function _transfer(address addressTo, uint256 amount) external returns (bool) {
-        // Snaity check
-        require(addressTo != address(0), "ERC20 transfer to addres 0.");
-        require(amount <= totalSupply, "Insufficient balance.");
-        // local variables 
-        totalSupply -= amount;
-        users[addressTo] += amount;
-        // code(s)
-        return true;
+    function transfer(address recipient, uint256  amount) external returns (bool){
+        return _transfer(msg.sender, recipient, amount);
     }
     /*
     function transferFrom()...
     this function allows a certian amount of token 
     to be transferred from one address to another
     */
-    function transferFrom(address addressFrom, address addressTo, uint256 amount) external  payable  {
+    function transferFrom(address addressFrom, address addressTo, uint256 amount) external returns (bool){
         // local variables 
-        uint256 currentBalance = users[addressFrom];
-        uint256 receiver = users[addressTo];
-
         uint256 currentAllowance = allowance[addressFrom][msg.sender];
         // Snaity check
-        require(currentAllowance >= amount, "ERC20 transfer amount excedd allowance."); 
-
-        require(amount <= currentBalance, "Insufficient balance.");
+        require(currentAllowance >= amount, "ERC20 transfer amount not exceed allowance."); 
         require(addressTo != address(0), "ERC20 transfer to addres 0.");
         // code(s)
-        currentAllowance -= amount;
-
-        receiver += amount;
-        currentBalance -= amount;
+        currentAllowance = currentAllowance - amount;
+        return _transfer(addressFrom, addressTo, amount);
     }
 
     function approve(address addressSpender, uint256 value) external returns (bool) {
