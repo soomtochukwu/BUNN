27663d3e8e9c77afce2129d476f79df2b769f6d8
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2023-08-20 12:42:53 +0100

    done

--- README.txt
+++ README.txt
@@ -1,1 +1,74 @@
-Somtochukwu Kelechi Onwuajuese 😎
+Hello and GM everyone
+
+My name is Somtohukwu Kelechi Onwuajuese
+
+This is in fulfillment of the first Web3 Bootcamp Assignment.
+
+Overall, this contract implements basic functionalities of an ERC-20 token,
+including transferring tokens, approving allowances, and maintaining balances.
+It demonstrates concepts like mappings, state variables, modifiers, and event
+emission commonly used in Ethereum smart contract development.
+
+
+Here's a brief description of its main components and functionalities:
+
+The contract has several state variables such as:
+
+NAME: Represents the token name (e.g., "Onwuajuese").
+
+SYMBOL: Represents the token symbol (e.g., "BUNN").
+
+DECIMAL: Indicates the number of decimal places for the token (e.g., 18).
+
+TOTALSUPPLY: Keeps track of the total supply of the token.
+
+OWNER: Stores the address of the contract owner.
+
+USERS: A mapping that associates addresses with token balances.
+
+ALLOWANCE: A nested mapping that tracks the approved allowances for transferring
+tokens between addresses.
+
+
+Its functionalities include:
+
+The CONSTRUCTOR function: it initializes the contract by setting the owner
+address and minting 1000e18 tokens to it.
+ 
+
+The ALLOW function: this function allows me to determine the amount of funds a
+particular address can transfer on behalf of another. In other words, it assigns
+value to the ALLOWANCE MAP at line 13. Its more like the approve function but more dynamic.
+ 
+
+The BALANCEOF: it allows an instance of the contract to store and return the balance of
+the provided address(user)
+
+
+The _MINT function is used by the contract owner to create new tokens and increase the total supply.
+ 
+
+The BURN function: it is supposed to allows the contract owner to decrease the 
+total supply by burning a certain amount of tokens. I also added a Burn Event to be emmited
+whenever the function called. 
+ 
+
+The _TRANSFER function is a private function that reduces code duplication by
+handling token transfers between addresses. In my own words, it acts as a
+TEMPLATE for the transfer FUNCTIONALITIES of the contract
+ 
+
+The TRANSFER function allows the caller to transfer tokens to another address.
+ 
+
+The TRANSFERFROM function allows an address to transfer tokens on behalf of
+another address, given that an allowance is set.
+ 
+
+The APPROVE function allows an address to approve another address to spend a
+certain number of tokens on its behalf.
+ 
+
+The contract emits two events: TRANSFER and APPROVAL, which provide
+information about token transfers and allowance approvals. It is meant to
+change the state of the EVM
--- contracts/BUNN_Token.sol
+++ contracts/BUNN_Token.sol
@@ -3,23 +3,32 @@ pragma solidity ^0.8.7;
 
 contract Onwuajuese {
     // state variables
-
     string public name = "Onwuajuese";
     string public symbol = "BUNN";
     uint8 public  decimal = 18;
     uint256 public  totalSupply;
-
+    /*
+    I think the owner variable should be privtae because
+    no function outside of the contact will make use of it.
+    */
+    address private owner;
     mapping (address => uint256) private users; // alias: "BalanceOf" ... 
     mapping (address => mapping (address => uint256)) public allowance;
-
+    // Event(s) definition(s)
+    event Transfer(address indexed indexedFrom, address indexed indexedTo, uint256 value);
+    event Approval(address indexed indexedOwner, address indexed indexedSpender, uint256 value);
+    event Burn(address indexed indexedOwner, uint256 amount);
+    // 
     constructor() {
-        _mint(msg.sender, 1234e18);
+        owner = msg.sender;
+        _mint(msg.sender, 1000e18);
     }
     /*
     function allow()...
     this function allows me to determine the amount of funds a
-    particula addres can transfer unbehalf of another.
-    In other words, it assigns value to the ALLOWANCE MAP at line 13
+    particular address can transfer unbehalf of another.
+    In other words, it assigns value to the ALLOWANCE MAP at line 18.
+    Its more like the approve function but more dynamic.
     */ 
     function allow(address addressFrom, address addressTO, uint256 allowed ) public  {
         allowance[addressFrom][addressTO] = allowed;
@@ -33,13 +42,31 @@ contract Onwuajuese {
         // code(s)
         return users[_user];
     }
-
-    function _mint(address address_to, uint256 amount ) internal  {
+    /*
+    function _mint()...
+    I think its reponsible for liquidity or creating of value.
+    */
+    function _mint(address address_to, uint256 amount ) public  {
         // Snaity check
-        require(address_to != address(0), "Must not be a zero address.");
+        require(address_to != address(0), "MUST NOT BE A ZERO ADDRESS.");
+        require(msg.sender == owner, "YOU ARE NOT AUTHORIZED TO MINT THIS TOKEN");
         // code(s)
         totalSupply += amount;
         users[address_to] += amount;
+        emit Transfer(msg.sender, address_to, amount);
+    }
+    /*
+    function burn()..
+    Enables the owner of the contract to burn a certain
+    amount of token from the total supply.
+    */
+    function burn(address _owner, uint256 amount) public  {
+        // Snaity check: [Using the require statement ensure that only the owner can call the _burn function]
+        require(_owner == owner, "NOT AUTHORIZED.");
+        // code(s)
+        users[_owner] -= amount;
+        totalSupply -= amount;
+        emit Burn(_owner, amount);
     }
     /*
     function _transfer()...
@@ -49,11 +76,12 @@ contract Onwuajuese {
         // local variables 
         uint256 currentBalance = users[addressFrom];
         // Snaity check
-        require(addressTo != address(0), "ERC20 transfer to addres 0.");
-        require(amount <= currentBalance, "Insufficient balance.");
+        require(addressTo != address(0), "ERC20 TRANSFER TO ADDRESS 0.");
+        require(amount <= currentBalance, "INSUFFICIENT BALANCE.");
         // code(s)
         users[addressFrom] -= amount; // currentAllowance -= amount; ... doeas not work
         users[addressTo] += amount;
+        emit Transfer(addressFrom, addressTo, amount);
         return true;
     }
     /*
@@ -73,19 +101,24 @@ contract Onwuajuese {
         // local variables 
         uint256 currentAllowance = allowance[addressFrom][msg.sender];
         // Snaity check
-        require(currentAllowance >= amount, "ERC20 transfer amount not exceed allowance."); 
-        require(addressTo != address(0), "ERC20 transfer to addres 0.");
+        require(currentAllowance >= amount, "ERC20 TRANSFER AMOUNT NOT EXCEED ALLOWANCE."); 
+        require(addressTo != address(0), "ERC20 TRANSFER TO ADDRES 0.");
         // code(s)
         currentAllowance = currentAllowance - amount;
+        emit Approval(addressFrom, addressTo, amount);
         return _transfer(addressFrom, addressTo, amount);
     }
-
+    /*
+    function approve()...
+    allows an address to approve another address to spend
+    a certain amount of tokens on its behal
+    */
     function approve(address addressSpender, uint256 value) external returns (bool) {
-        require(addressSpender == msg.sender, "not approved.");
+        // Snaity check
+        require(addressSpender != address(0), "INVALID ADDRESS!");
+        // code(s)
+        allowance[msg.sender][addressSpender];
+        emit Approval(msg.sender, addressSpender, value);
         return true;
     }
-
-    event Transfer(address indexedFrom, address indexedTo, uint256 value);
-
-    event Approval(address indexedOwner, address indexedSpender, uint256 value);
 }
